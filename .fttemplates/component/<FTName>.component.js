5691471749b9aca76b98cc6dbc7143b728bca66e
Mehedov <mehedov.dev@yandex.ru>
2024-09-17 20:33:36 +0300

    Quick template creation

---
+++ .fttemplates/component/<FTName>.component.js
@@ -0,0 +1,12 @@
+import ChildComponent from '@/core/component/child.component.js'
+import renderService from '@/core/service/render.service.js'
+
+import styles from './<FTName>.module.scss'
+import template from './<FTName>.template.html'
+
+export class <FTName | pascalcase> extends ChildComponent {
+	render() {
+		this.element = renderService.htmlToElement(template, [], styles)
+		return this.element
+	}
+}
