e1872a5d1d0bd3731a06644f79e3e1ea0eec31b0
Mehedov <mehedov.dev@yandex.ru>
2024-09-17 20:11:33 +0300

    Render template component

--- src/components/screens/home/home.component.js
+++ src/components/screens/home/home.component.js
@@ -1,12 +1,15 @@
 import { BaseScreen } from '@/core/component/base-screen.component'
-
+import renderService from '@/core/service/render.service'
+import * as style from './home.module.scss'; 
+import template from './home.template.html'
 
 export class Home extends BaseScreen {
 	constructor() {
-		super({title: 'Home'})
+		super({ title: 'Home' })
 	}
 
 	render() {
-		return '<div>Home</div>'
+		const element = renderService.htmlToElement(template, [], style)
+		return element.outerHTML
 	}
 }
