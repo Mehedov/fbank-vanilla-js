1900c689cea82480c5f46943fb1634c371fcd509
Mehedov <mehedov.dev@yandex.ru>
2024-09-21 23:24:42 +0300

    Checking the UI

--- src/components/screens/home/home.component.js
+++ src/components/screens/home/home.component.js
@@ -1,8 +1,10 @@
+import { Field } from '@/components/UI/field/field.component'
 import { BaseScreen } from '@/core/component/base-screen.component'
 import { $F } from '@/core/fquery/fquery.lib'
 import renderService from '@/core/service/render.service'
-import * as style from './home.module.scss'
+import * as styled from './home.module.scss'
 import template from './home.template.html'
+import { Button } from '@/components/UI/button/button.component'
 
 export class Home extends BaseScreen {
 	constructor() {
@@ -10,7 +12,22 @@ export class Home extends BaseScreen {
 	}
 
 	render() {
-		const element = renderService.htmlToElement(template, [], style)
+		const element = renderService.htmlToElement(
+			template,
+			[
+				new Field({
+					name: 'test',
+					placeholder: 'Search',
+					variant: 'green'
+				}),
+				new Button({
+					children: 'Button',
+					onClick: () => alert('CLICK!!!'),
+					variant: 'red'
+				})
+			],
+			styled
+		)
 
 		$F(element).find('h1').css('color', 'red')
 
