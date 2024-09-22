57d74ab93809a2f6f1efe89eb3a4ba586ad56839
Mehedov <mehedov.dev@yandex.ru>
2024-09-22 15:18:16 +0300

    fquery.lib.js: Adding new functions to FQuery: attr, text

--- src/core/fquery/fquery.lib.js
+++ src/core/fquery/fquery.lib.js
@@ -85,6 +85,23 @@ class FQuery {
 	}
 
 	/**
+	 * Get or set the text content of the selected element.
+	 * @param {string} [textContent] - Optional text content to set. If
+	 * not provided, the current text content will be returned.
+	 * @returns {FQuery/string} The current FQuery instance for
+	 * chaining when setting text content, or the current text content
+	 * when getting.
+	 */
+	text(textContent) {
+		if (typeof textContent === 'undefined') {
+			return this.element.textContent
+		} else {
+			this.element.textContent = textContent
+			return this
+		}
+	}
+
+	/**
 	 * Attach a click event listener to the selected element.
 	 * @param {function(Event): void} callback - The event listener
 	 * function to execute when the selected element is clicked. The
@@ -201,6 +218,29 @@ class FQuery {
 			}
 		} else {
 			this.element.classList.remove(classNames)
+		}
+
+		return this
+	}
+
+	/**
+	 * Set or get the value of an attribute on the selected element.
+	 * @param {string} attributeName - The name of the attribute to set
+	 * or get.
+	 * @param {string} [value] - The value to set for the attribute. If
+	 * not provided, the current value of the attribute will be returned.
+	 * @returns {ROuery/string} The current RQuery instance for
+	 * chaining (if setting) or the attribute value (if getting).
+	 */
+	attr(attributeName, value) {
+		if (typeof attributeName !== 'string') {
+			throw new Error('Attribute name must be a string')
+		}
+
+		if (typeof value === 'undefined') {
+			return this.element.getAttribute(attributeName)
+		} else {
+			this.element.setAttribute(attributeName, value)
 		}
 
 		return this
